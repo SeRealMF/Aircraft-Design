27245f189789fe46b395081027f39e3cf96cb137
pepegarnic <147069557+pepegarnic@users.noreply.github.com>
2024-11-25 18:21:13 +0100

    Landing_Distance more

--- Landing_Distance.py
+++ Landing_Distance.py
@@ -1,5 +1,8 @@
-#sL =s50 + sR
 
+
+
+#sL =s50 + sR
+#W_L...Wingloading
 
 g_0 = 9.81 #m/s
 #Energie approach: W_L*v_50*V_50/(2*g_0) + W_L*h_50
@@ -10,10 +13,24 @@ decelleration Energy = W_L*v_50*V_50/(2*g_0) + W_L*h_50 - W_L*v_L*V_50/(2*g_0)
 
 s_50 = (W_L/D)*(1/(2*g_0))*(v_50*v_50-v_l*v_L)+(W_L/D)*h_50
 
+v_S = sqrt((2/rho*c_Lmac_Ldg)*(W_L/S)) = sqrt((2/rho*c_Lmac_Ldg)*(W_L/S))*sqrt(rho_0/rho)
+
+v_L=1.13*v_S
+v_50 = 1.23*v_S
 
 
+
+
+
+s_R=-v_L*v_L/(2*b_M) = (-W_L/S)*
 sL=s_50+s_R
 S_Lops = s_L*(1/0.7)
 return (S_Lops)
+
+#Taking into account the operational rule that the approach speed must be 23% higher than the minimum
+#speed for steady-state flight, a given maximum approach speed results in a maximum landing area load of
+
+(W_L/S)_max = (Rho/2)*c_lmax_L*(v_50/multiplicati)
+
 https://tuwel.tuwien.ac.at/pluginfile.php/4235978/mod_resource/content/0/2024_11_04_propulsion_systems_dimensioning.pdf
 
