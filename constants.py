a5b8d27c70961cf91b651c6659d8a1ad7f6eb3f8
timminador <t.fengler@gmx.at>
2024-12-12 11:59:23 +0100

    changed calculation of cruise velocity

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #General
 Wto = 90000 #maxium takeoff weight base version in kg
 M_CRUISE = 0.8 #Mach number during cruise
-#V_CRUISE = 140 #crusing speed in m/s
+# V_CRUISE = 140 #crusing speed in m/s - disused calc through general calc from mach
 ntrans = 0.99 #transmission efficency (gearbox)
 nprop = 0.9 #propeller efficeny, variable pitch.
 AR = 10 #Aspect Ratio
@@ -10,9 +10,9 @@ H_CRUISE = 12200 #crusing altitude in m
 H_CRUISE_OEI = 3050 #crusing altidude OEI in m
 N_E = 2 #number of engines
 N_prop = 3 #number of Blades on the Prop
-#ToOEI = 80000 #XXX NOT RIGHT VALUE XXX
-SeCe = 40000 #Service Ceiling CHECK VALUE!!!!
-vvre =  100 # Minimum Vertikal Speed in ft/min CHECK VALUE!!!!
+ToOEI = 80000 #XXX NOT RIGHT VALUE XXX
+SeCe = 14000 #Service Ceiling CHECK VALUE!!!!
+vvre =  300 # Minimum Vertikal Speed in ft/min CHECK VALUE!!!!
 dt = 0  # no temperature difference Temperature(ISA) CHECK VALUE!!!!
 ma = 0.75  # chosen Mach number for flight CHECK VALUE!!!!
 n_zw = 1 #Load Multiplier Vertical Trajectory
@@ -57,6 +57,7 @@ v_50 = 1.23*v_s #The approach speed must be 23% higher than the minimum speed fo
 v_approach = v_50
 if 2<=N_E<=3: v_L =  1.13*v_s
 if N_E>3: v_L =  1.08*v_s
+epsilon_L = 5#Lift, to Discuss
 S = 250 #Wing Surface Area, to Dicuss
 
 b_M = -9.81/3 #Braking deacceleration [m/s^2], g/2 is appropriate
